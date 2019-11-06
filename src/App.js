5b6fa1b5f374ef6e8f958a47a66123064df6defe
ozvaris <ozgurozvaris@hotmail.com>
2019-11-07 01:59:19 +0300

    hover control added to button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person'
 
 class App extends Component {
@@ -63,7 +64,11 @@ class App extends Component {
       font:'white',
       border:'1px solid blue',
       padding:'8px',
-      cursor:'pointer'
+      cursor:'pointer',
+      ':hover':{
+        backgroundColor : 'lightgreen',
+        color:'black'
+      }
 
     };
 
@@ -86,6 +91,10 @@ class App extends Component {
       )
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor : 'salmon',
+        color:'black'
+      }
 
     }
     
@@ -117,4 +126,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
